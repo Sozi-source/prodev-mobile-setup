5c6bf469165c96620369dbbfd1613c6ea2bd137e
Wilfred Osozi <osoziw@gmail.com>
2025-08-30 19:42:32 +0300

    updated index

--- prodev-mobile-app-0x02/app/index.tsx
+++ prodev-mobile-app-0x02/app/index.tsx
@@ -6,7 +6,7 @@ export default function Index() {
   return(
     <SafeAreaProvider>
       <SafeAreaView style={{flex:1}}>
-        <ImageBackground source={require("../assets/images/background-image.png")}
+        <ImageBackground source={require("@/assets/images/background-image.png")}
           style={styles.background} resizeMode="cover">
             <View>
               <Image source={require("../assets/images/Logo.png")} style={styles.companyLogo}/>
